a2e1aaa32330bccdbc87de9bc52a506f0e78c116
seunggyu <nike4434@gmail.com>
2020-07-28 16:18:40 +0900

    C# DB connection

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputbox = new System.Windows.Forms.TextBox();
+            this.searchbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.namebox = new System.Windows.Forms.TextBox();
+            this.agebox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(-2, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 170);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(504, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "조회";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // inputbox
+            // 
+            this.inputbox.Location = new System.Drawing.Point(398, 10);
+            this.inputbox.Name = "inputbox";
+            this.inputbox.Size = new System.Drawing.Size(100, 25);
+            this.inputbox.TabIndex = 2;
+            // 
+            // searchbox
+            // 
+            this.searchbox.Location = new System.Drawing.Point(384, 63);
+            this.searchbox.Multiline = true;
+            this.searchbox.Name = "searchbox";
+            this.searchbox.Size = new System.Drawing.Size(195, 107);
+            this.searchbox.TabIndex = 3;
+            this.searchbox.TextChanged += new System.EventHandler(this.searchbox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "이름으로 조회하기";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(985, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 57);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "조회";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // namebox
+            // 
+            this.namebox.Location = new System.Drawing.Point(867, 64);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(100, 25);
+            this.namebox.TabIndex = 6;
+            // 
+            // agebox
+            // 
+            this.agebox.Location = new System.Drawing.Point(867, 95);
+            this.agebox.Name = "agebox";
+            this.agebox.Size = new System.Drawing.Size(100, 25);
+            this.agebox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(864, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "DB에 추가하기";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 531);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.agebox);
+            this.Controls.Add(this.namebox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchbox);
+            this.Controls.Add(this.inputbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inputbox;
+        private System.Windows.Forms.TextBox searchbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox namebox;
+        private System.Windows.Forms.TextBox agebox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public static String url = "Server=localhost;Database=member1;Uid=root;Pwd=0000;charset = utf8";
+        private MySqlConnection mConnection;
+        private MySqlCommand mCommand;
+        private MySqlDataReader mDataReader;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mConnection = new MySqlConnection(url); // DB접속
+            mCommand = new MySqlCommand(); // 쿼리문 생성
+            mCommand.Connection = mConnection; // DB에 연결
+            mCommand.CommandText = "SELECT * FROM student"; // 쿼리문 작성
+            mConnection.Open(); // DB 오픈
+            mDataReader = mCommand.ExecuteReader(); // 쿼리문 실행
+
+            while (mDataReader.Read()) // 전부 다 읽어 옴
+            {
+                // 여기서 부터 원하는 데이터를 받아와서 처리
+                string name = mDataReader["name"].ToString();
+                string age = mDataReader["age"].ToString();
+                textBox1.AppendText("이름 : " + name);
+                textBox1.AppendText("\t나이 : " + age + Environment.NewLine);
+            }
+            mConnection.Close(); // 사용 후 객체 닫기
+        }
+
+
+
+        //조회 + 풀 네임
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //searchbox.Text = inputbox.Text;
+            mConnection = new MySqlConnection(url); // DB접속
+            mCommand = new MySqlCommand(); // 쿼리문 생성
+            mCommand.Connection = mConnection; // DB에 연결
+            mCommand.CommandText = "SELECT * FROM student WHERE name = "+"'"+inputbox.Text+"'"; // 쿼리문 작성
+            mConnection.Open(); // db on
+            mDataReader = mCommand.ExecuteReader();
+
+            while (mDataReader.Read())
+            {   // : 전체 값 읽어서 출력 
+               // Console.WriteLine(mDataReader[0] + "----" + mDataReader[1]);
+                string name = mDataReader["name"].ToString();
+                string age = mDataReader["age"].ToString();
+                searchbox.AppendText("이름 : " + name);
+                searchbox.AppendText("\t나이 : " + age + Environment.NewLine);
+            }
+            MessageBox.Show((String msg) "Hello Mablang World!",(String title) "Mablang", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            mConnection.Close();
+        }
+
+        // 삽입
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mConnection = new MySqlConnection(url); // DB접속
+            mCommand = new MySqlCommand(); // 쿼리문 생성
+            mCommand.Connection = mConnection; // DB에 연결
+            mCommand.CommandText = "insert into student(name,age) values(" + "'" + namebox.Text + "," + "'" + agebox.Text + "')";
+            mConnection.Open();
+            mDataReader = mCommand.ExecuteReader();
+            //들어간 값 확인하기
+            while (mDataReader.Read())
+            {
+               // MessageBox.Show((String msg) "Hello Mablang World!",(String title) "Mablang", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                string name = mDataReader["name"].ToString();
+                string age = mDataReader["age"].ToString();
+                searchbox.AppendText("이름 : " + name);
+                searchbox.AppendText("\t나이 : " + age + Environment.NewLine);
+            }
+            mConnection.Close();
+        }
+
+        private void searchbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
